71a8e56f85360d84b5e6660fa3416d5b01604f7c
zmokni <zmokni@norauto.com>
2024-12-15 12:09:49 +0100

    :bug: delete button

--- src/main/java/adeo/leroymerlin/cdp/AdeoLeroyMerlinCDPRecruitmentApplication.java
+++ src/main/java/adeo/leroymerlin/cdp/AdeoLeroyMerlinCDPRecruitmentApplication.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 public class AdeoLeroyMerlinCDPRecruitmentApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdeoLeroyMerlinCDPRecruitmentApplication.class, args);
 	}
